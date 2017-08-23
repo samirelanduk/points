e01171ba2530d10d7d59494318692b339ea255f2
Sam Ireland <sam.ireland.uk@gmail.com>
2017-08-23 14:47:27 +0100

    Import transformations

---
+++ points/transform.py
@@ -0,0 +1,100 @@
+"""Contains functions for manipulating Cartesian coordinates."""
+
+from math import radians, sin, cos
+from .checks import are_numeric, is_numeric
+from .matrices import Matrix
+from .vectors import Vector
+
+def accept_objects(func):
+    """This decorator can be applied to functions whose first argument is a list
+    of x, y, z points. It allows the function to also accept a list of objects
+    which have x(), y() and z() methods instead."""
+
+    def new_func(objects, *args, **kwargs):
+        try:
+            points = [(x, y, z) for x, y, z in objects]
+        except TypeError:
+            points = [(obj.x(), obj.y(), obj.z()) for obj in objects]
+        return func(points, *args, **kwargs)
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+    return new_func
+
+
+@accept_objects
+def translate(points, x, y, z):
+    """Takes a set of coordinates and translates them in three dimensional
+    space.
+
+    The points must be a list (or tuple, or any collection really) of
+    coordinates in the form ``(x, y, z)``, *or* a list (etc.) of objects with
+    x(), y() and z() methods.
+
+    An example would be ``translate([(1, 1, 1), (2, 2, 2)], 5, 5, 5)``.
+
+    :param points: A collection of (x, y, z) coordinates or appropriate objects.
+    :param number x: The distance to move the points in the x direction.
+    :param number y: The distance to move the points in the y direction.
+    :param number z: The distance to move the points in the z direction.
+    :returns: The translated coordinates."""
+
+    if not are_numeric(x, y, z):
+        raise TypeError("Translation parameters must be numeric, not '%s'" % (
+         str((x, y, z))
+        ))
+    return tuple([tuple([px + x, py + y, pz + z]) for px, py, pz in points])
+
+
+@accept_objects
+def rotate(points, axis, angle, hand="right"):
+    """Takes a set of coordinates and rotates them around one of the axes by a
+    specified angle. The rotation performed is right-handed, unless specified
+    otherwise.
+
+    The points must be a list (or tuple, or any collection) of
+    coordinates in the form ``(x, y, z)``, *or* a list (etc.) of objects with
+    x(), y() and z() methods.
+
+    An example would be ``rotate([(1, 1, 1), (2, 2, 2)], "x", 45)``.
+
+    :param points: A collection of (x, y, z) coordinates or appropriate objects.
+    :param str axis: The axis to rotate around. Accepted values are `"x"`,\
+    `"y"` or `"z"`.
+    :param number angle: The angle in degrees to rotate by.
+    :param str hand: specifies whether the rotation should be right-handed or\
+    left-handed. The deafult is 'right'.
+    :returns: The rotated coordinates."""
+
+    if not is_numeric:
+        raise TypeError("angle must be numeric, not '%s'" % str(angle))
+    if not isinstance(hand, str):
+        raise TypeError("hand must be str, not '%s'" % str(hand))
+    elif hand not in ("left", "right"):
+        raise ValueError("hand must be 'left' or 'right', not %s" % hand)
+    elif hand == "left":
+        angle = -angle
+    angle = radians(angle)
+    points = [create_vertex(*point) for point in points]
+    matrix = None
+    if axis == "x":
+        matrix = Matrix(
+         (1, 0, 0),
+         (0, cos(angle), -sin(angle)),
+         (0, sin(angle),  cos(angle))
+        )
+    elif axis == "y":
+        matrix = Matrix(
+         (cos(angle), 0, sin(angle)),
+         (0, 1, 0),
+         (-sin(angle), 0, cos(angle))
+        )
+    elif axis == "z":
+        matrix = Matrix(
+         (cos(angle), -sin(angle), 0),
+         (sin(angle), cos(angle), 0),
+         (0, 0, 1)
+        )
+    else:
+        raise ValueError("axis can only be 'x', 'y' or 'z', not %s" % axis)
+    new_points = [matrix * point for point in points]
+    return tuple([point.columns()[0] for point in new_points])
---
+++ tests/unit/test_transformations.py
@@ -0,0 +1,131 @@
+from unittest import TestCase
+from unittest.mock import Mock
+from points.transform import translate, rotate, accept_objects
+
+class TransformationTest(TestCase):
+
+    def setUp(self):
+        self.points = [(1, 1, 1), (2, 1, 1), (3, 1, 1), (4, 1, 1), (5, 1, 1)]
+
+
+    def assertPointsAlmostEqual(self, points1, points2):
+        self.assertEqual(len(points1), len(points2))
+        for index, point1 in enumerate(points1):
+            point2 = points2[index]
+            self.assertEqual(len(point1), len(point2), 3)
+            self.assertAlmostEqual(point1[0], point2[0], delta=0.0005)
+            self.assertAlmostEqual(point1[1], point2[1], delta=0.0005)
+            self.assertAlmostEqual(point1[2], point2[2], delta=0.0005)
+
+
+
+class TranslationTests(TransformationTest):
+
+    def test_can_translate(self):
+        self.assertEqual(
+         translate(self.points, 3, -2, 8),
+         ((4, -1, 9), (5, -1, 9), (6, -1, 9), (7, -1, 9), (8, -1, 9))
+        )
+
+
+    def translation_directions_must_be_numeric(self):
+        with self.assertRaises(TypeError):
+            translate(self.points, "3", -2, 8)
+        with self.assertRaises(TypeError):
+            translate(self.points, 3, "-2", 8)
+        with self.assertRaises(TypeError):
+            translate(self.points, 3, -2, "8")
+
+
+
+class RotationTests(TransformationTest):
+
+    def test_can_rotate_around_x_axis_180(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "x", 180),
+         ((1, -1, -1), (2, -1, -1), (3, -1, -1), (4, -1, -1), (5, -1, -1))
+        )
+
+
+    def test_can_rotate_around_x_axis_90(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "x", 90),
+         ((1, -1, 1), (2, -1, 1), (3, -1, 1), (4, -1, 1), (5, -1, 1))
+        )
+
+
+    def test_can_rotate_around_y_axis_180(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "y", 180),
+         ((-1, 1, -1), (-2, 1, -1), (-3, 1, -1), (-4, 1, -1), (-5, 1, -1))
+        )
+
+
+    def test_can_rotate_around_y_axis_90(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "y", 90),
+         ((1, 1, -1), (1, 1, -2), (1, 1, -3), (1, 1, -4), (1, 1, -5))
+        )
+
+
+    def test_can_rotate_around_z_axis_180(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "z", 180),
+         ((-1, -1, 1), (-2, -1, 1), (-3, -1, 1), (-4, -1, 1), (-5, -1, 1))
+        )
+
+
+    def test_can_rotate_around_z_axis_90(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "z", 90),
+         ((-1, 1, 1), (-1, 2, 1), (-1, 3, 1), (-1, 4, 1), (-1, 5, 1))
+        )
+
+
+    def test_can_switch_handedness(self):
+        self.assertPointsAlmostEqual(
+         rotate(self.points, "x", 90, hand="left"),
+         ((1, 1, -1), (2, 1, -1), (3, 1, -1), (4, 1, -1), (5, 1, -1))
+        )
+
+
+    def test_axis_must_be_x_or_y_or_z(self):
+        with self.assertRaises(ValueError):
+            rotate(self.points, "a", 90)
+
+
+    def test_angle_must_be_numeric(self):
+        with self.assertRaises(TypeError):
+            rotate(self.points, "x", "90")
+
+
+    def test_hand_must_be_str(self):
+        with self.assertRaises(TypeError):
+            rotate(self.points, "x", 90, hand=100)
+
+
+    def test_hand_must_be_valid(self):
+        with self.assertRaises(ValueError):
+            rotate(self.points, "x", 90, hand="wrong")
+
+
+
+class ObjectAcceptanceDecoratorTests(TestCase):
+
+    def setUp(self):
+        def func(points, arg1, arg2, arg3):
+            return [(x + arg1, y + arg2, z + arg3) for x, y, z in points]
+        self.func = func
+
+
+    def test_can_apply_decorator(self):
+        new_func = accept_objects(self.func)
+        obj1, obj2 = Mock(), Mock()
+        obj1.x.return_value, obj1.y.return_value, obj1.z.return_value = 1, 1, 1
+        obj2.x.return_value, obj2.y.return_value, obj2.z.return_value = 2, 2, 2
+        self.assertEqual(new_func([obj1, obj2], 3, 4, 5), [(4, 5, 6), (5, 6, 7)])
+
+
+    def test_decorator_ignores_already_fine_points(self):
+        new_func = accept_objects(self.func)
+        self.assertEqual(new_func([(1, 1, 1), (2, 2, 2)], 3, 4, 5), [(4, 5, 6), (5, 6, 7)])
