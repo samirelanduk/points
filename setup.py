1adbcdd1b36562085ecb353b067e9630f6f79552
Sam Ireland <sam.ireland.uk@gmail.com>
2017-08-23 14:33:51 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+ name="points",
+ version="0.1.0",
+ description="A pure-Python geometry and linear algebra library",
+ url="https://points.samireland.com",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Science/Research",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Scientific/Engineering :: Mathematics",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.0",
+  "Programming Language :: Python :: 3.1",
+  "Programming Language :: Python :: 3.2",
+  "Programming Language :: Python :: 3.3",
+  "Programming Language :: Python :: 3.4",
+  "Programming Language :: Python :: 3.5",
+  "Programming Language :: Python :: 3.6",
+ ],
+ keywords="geometry trigonometry vectors matrices linear-algebra",
+ packages=["points"]
+)
