4e82dcaf96faff40cc7d59c5bc14b7ec2b40454c
Sam Ireland <sam.ireland.uk@gmail.com>
2017-09-04 13:20:02 +0100

    Add vector integration test

--- points/__init__.py
+++ points/__init__.py
@@ -1,2 +1,5 @@
 __author__ = "Sam Ireland"
 __version__ = "0.1.0"
+
+from .checks import is_numeric, are_numeric
+from .vectors import Vector
---
+++ tests/integration/test.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+import points
+
+class VectorTests(TestCase):
+
+    def test_vectors(self):
+        v1 = points.Vector(5, 23, 17)
+        v2 = points.Vector([14, -4, 9])
+
+        self.assertIn(23, v1)
+        self.assertNotIn(23, v2)
+        self.assertEqual(v1[1], 23)
+
+        self.assertEqual(v1.values(), (5, 23, 17))
+        v1.add(9)
+        self.assertEqual(v1.values(), (5, 23, 17, 9))
+        v1.remove(9)
+        self.assertEqual(v1.values(), (5, 23, 17))
+
+        self.assertAlmostEqual(v1.magnitude(), 29.034, delta=0.005)
+        self.assertAlmostEqual(v1.angle_with(v2), 1.304, delta=0.005)
+
+        v = v1 + v2
+        self.assertEqual(v.values(), (19, 19, 26))
+        v = v1 - v2
+        self.assertEqual(v.values(), (-9, 27, 8))
+        v = v1 * 10
+        self.assertEqual(v.values(), (50, 230, 170))
+        v = v1.dot(v2)
+        self.assertEqual(v, 131)
